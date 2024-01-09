52e08460f6d06764c59159d8e49979e9dad8140b
karolI2000 <kiwanow3043@student.pps.net>
2024-01-09 13:51:39 -0800

    1/9/24

--- Assets/Scripts/PlayerFireball.cs
+++ Assets/Scripts/PlayerFireball.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerFireball : MonoBehaviour
 {
+    public UnityEngine.Vector3 target;
+    private float timer;
+    private float originalTimer;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Enemy");
+        originalTimer = 3;
+        timer = originalTimer;
+        target = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            gameObject.SetActive(false);
+        }
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(current: transform.position,target, 0.0025f);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerTargetting.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTargetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject fireball;
+    private Collider2D target;
+    private float originalFiringDelay;
+    private float firingDelay;
+    private Vector3 ySpawn;
+    void Start()
+    {
+        target = null;
+        originalFiringDelay = 1f;
+        firingDelay = originalFiringDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        firingDelay -= Time.deltaTime;
+        if (firingDelay < 0)
+        {
+            firingDelay = originalFiringDelay;
+            if (target != null)
+            {
+                ySpawn = transform.position;
+                ySpawn.y += 1;
+                Instantiate(fireball, ySpawn, transform.rotation);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            target = other;
+            Debug.Log("Targetting Enemy");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            target = null;
+            firingDelay = originalFiringDelay;
+            Debug.Log("Targetting Stopped");
+        }
+    }
+}
